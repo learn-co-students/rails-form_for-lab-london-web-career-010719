b7885137208281731e17e88c193031df47df489b
github username <github email address>
2019-02-02 17:50:48 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,43 @@
+
+class StudentsController < ApplicationController
+  before_action :find_student, only: [:edit, :show, :update, :destroy]
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+    redirect_to student
+  end
+
+  def edit
+  end
+
+  def update
+    @student.update(student_params)
+    redirect_to @student
+  end
+
+  def destroy
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+end
--- spec/factories/school_classes.rb
+++ spec/factories/school_classes.rb
@@ -1,6 +1,7 @@
-FactoryBot.define do
-  factory :school_class do
-    title "Computer Science"
-    room_number 5
-  end
-end
+# FactoryBot.define do
+#   factory :school_class do
+#     title "Computer Science"
+#     room_number 5
+#   end
+# end
+#
